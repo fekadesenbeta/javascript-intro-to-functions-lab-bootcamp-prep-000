3511d999126f982438274c0744538dc32dbc818e
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-04-04 22:10:12 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(hello) {
+  console.log('HELLO')
+  return hello.toUpperCase
+}
+logShout(hello)
+
+function logWhisper(hello) {
+  console.log('hello')
+  return hello.toLowerCase
+}
+logWhisper(hello)
+
+
+function sayHiToGrandma(hello) {
+  var hello
+  if (hello === hello.toLowerCase() )
+  return hello.toLowerCase()
+}
+ 
+ 
