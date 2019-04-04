9020a77ac045fc021f6f664d0b2f580720da829b
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-04-04 23:02:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ function sayHiToGrandma(hello) {
 
 }
 
-function sayHiToGrandma(I love you, Grandma) {
- if (I love you, Grandma === hello)
+function sayHiToGrandma(hello) {
+ if (hello === "I love you, Grandma.")
  return "I love you, too."
 
 }
