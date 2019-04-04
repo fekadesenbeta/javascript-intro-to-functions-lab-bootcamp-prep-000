b86e692d9d7c9834dbef4b9d8c874a1f524a7714
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-04-04 22:20:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,8 +24,10 @@ function sayHiToGrandma(string) {
  return 'I can\'t hear you!';
 }
 
-function sayHiToGrandma(string){
+function sayHiToGrandma(string) {
   if (string.toUpperCase() === string )
+  return 'YES INDEED';
+}
 
 
 
